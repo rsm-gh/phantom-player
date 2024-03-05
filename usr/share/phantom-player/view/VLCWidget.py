f0f38f93e4540422a52eb527b2c85f2d58bcea5c
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-03-05 12:44:33 +0100

    Code structure & quality

--- usr/share/phantom-player/view/VLCWidget.py
+++ usr/share/phantom-player/view/VLCWidget.py
@@ -20,15 +20,9 @@
 #   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-import gi
-import os
 import sys
 import vlc
 import ctypes
-
-os.environ["GDK_BACKEND"] = "x11"
-
-gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 # Create a single vlc.Instance() to be shared by (possible) multiple players.
