b88569a2bf5344642588ba51aa744c1c95dc3c83
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-05-02 01:47:37 +0200

    New playlists files & format

--- usr/share/phantom-player/controller/playlist_factory.py
+++ usr/share/phantom-player/controller/playlist_factory.py
@@ -14,156 +14,345 @@
 #   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import os
-import csv
 
 from model.Playlist import Playlist
 from model.PlaylistPath import PlaylistPath
-from model.Video import VideoPosition
+from model.Video import VideoPosition, Video
 from Paths import _SERIES_DIR
-from controller.utils import str_to_boolean, read_lines
+
+_COLUMN_SEPARATOR = "|"
+_VALUE_SEPARATOR = "="
+
+# These must be either GET or SET
+_PLAYLIST_ATTR = ('random',
+                  'keep_playing',
+                  'start_at',
+                  'audio_track',
+                  'subtitles_track',
+                  'current_video_hash')
+
+_PLAYLIST_PATH_ATTR = ('recursive', 'startup_discover')
+_VIDEO_ATTR = ('position', 'ignore', 'path', 'name')
 
 
-def load_from_file(file_path, pid):
-    print("Loading headers & paths of ", file_path)
-    lines = read_lines(file_path)
+class SaveParams:
+    class Section:
+        _settings = "[SETTINGS]"
+        _sources = "[SOURCES]"
+        _videos = "[VIDEOS]"
 
-    #
-    # Read the header
-    #
-    random = False
-    keep_playing = False
-    start_at = VideoPosition._start
-    audio_track = 0
-    current_video_hash = ""
 
-    try:
-        header = lines[0].split("|")
-    except Exception:
-        print("\tError getting the header.")
-    else:
+def str_to_boolean(value):
+    return value.lower().strip() == "true"
 
-        try:
-            random = str_to_boolean(header[0])
-        except Exception:
-            print("\tError getting 'random'")
 
-        try:
-            keep_playing = str_to_boolean(header[1])
-        except Exception:
-            print("\tError getting 'keep_playing'")
+def load(file_path):
+    print("Loading...", file_path)
+    file_lines = __get_lines(file_path)
 
-        try:
-            start_at = float(header[2])
-        except Exception:
-            print("\tError getting 'start_at'")
+    playlist = Playlist()
+    playlist.set_name(os.path.basename(file_path))
 
-        try:
-            audio_track = int(header[3])
-        except Exception:
-            print("\tError getting 'audio_track'")
+    __load_settings(playlist, file_lines)
 
-        try:
-            subtitles_track = int(header[4])
-        except Exception:
-            print("\tError getting 'subtitles_track'")
-            subtitles_track = 0
+    for playlist_path in __load_paths(file_lines):
+        added = playlist.add_playlist_path(playlist_path)
+        if not added:
+            print('\tError rejected path=' + file_path)
 
-        try:
-            # Deprecated icon extension
-            _ = header[5].strip()
-        except Exception:
-            pass
+    __load_videos(playlist, file_lines)
 
-        try:
-            current_video_hash = header[6].strip()
-        except Exception:
-            print("\tError getting 'current_video_hash'")
-        else:
-            if len(current_video_hash) < 10:  # in case the CSV parser saves "false" as value
-                current_video_hash = ""
-
-    #
-    # Create the playlist (without loading the videos)
-    #
-    new_playlist = Playlist(pid=pid,
-                            name=os.path.basename(file_path),
-                            is_random=random,
-                            keep_playing=keep_playing,
-                            start_at=start_at,
-                            audio_track=audio_track,
-                            subtitles_track=subtitles_track,
-                            current_video_hash=current_video_hash)
-
-    #
-    # Read the path
-    #
-    for i, line in enumerate(lines):
-
-        if i == 0:
-            continue
-
-        possible_path = line.split("|")
-
-        if len(possible_path) != 3:
-            break
-
-        else:
-            recursive = False
-            r_startup = False
-
-            try:
-                data_path = possible_path[0].strip()
-            except Exception:
-                print("\tError getting 'data_path'")
-                continue
-            else:
-                if "/" not in data_path and "\\" not in data_path:
-                    print("\t\tinvalid the path=", data_path)
-                    continue
-
-            try:
-                recursive = str_to_boolean(possible_path[1])
-            except Exception:
-                print("\tError getting 'recursive'")
-
-            try:
-                r_startup = str_to_boolean(possible_path[2])
-            except Exception:
-                print("\tError getting 'r_startup'")
-
-            playlist_path = PlaylistPath(path=data_path,
-                                         recursive=recursive,
-                                         startup_discover=r_startup)
-            added = new_playlist.add_playlist_path(playlist_path)
-            if not added:
-                print('\tError rejected path=' + data_path)
-
-    return new_playlist
+    return playlist
 
 
 def save(playlist):
     if not os.path.exists(_SERIES_DIR):
         os.mkdir(_SERIES_DIR)
 
-    with open(playlist.get_save_path(), mode='wt', encoding='utf-8') as f:
-        csv_list = csv.writer(f, delimiter='|')
+    data = """
+#
+# Playlist file for Phantom-Player
+# https://github.com/rsm-gh/phantom-player
+#
+"""
 
-        csv_list.writerow([playlist.get_random(),
-                           playlist.get_keep_playing(),
-                           playlist.get_start_at(),
-                           playlist.get_audio_track(),
-                           playlist.get_subtitles_track(),
-                           "None",
-                           playlist.get_current_video_hash()])
+    #
+    # Add the playlist data
+    #
+    data += "\n\n{}\n\n".format(SaveParams.Section._settings)
+    for attr_name in _PLAYLIST_ATTR:
+        value = getattr(playlist, "get_" + attr_name)()
+        data += "{}={}\n".format(attr_name, value)
 
-        for playlist_path in playlist.get_playlist_paths():
-            csv_list.writerow([playlist_path.get_path(),
-                               playlist_path.get_recursive(),
-                               playlist_path.get_startup_discover()])
+    #
+    # Add the sources data
+    #
+    data += "\n\n{}\n\n".format(SaveParams.Section._sources)
+    for playlist_path in playlist.get_playlist_paths():
+        data += __join_line_attrs(playlist_path.get_path(), playlist_path, _PLAYLIST_PATH_ATTR)
 
-        for video in playlist.get_videos():
-            csv_list.writerow([video.get_path(),
-                               video.get_name(),
-                               video.get_position(),
-                               video.get_ignore(),
-                               video.get_hash()])
+    #
+    # Add the videos data
+    #
+    data += "\n\n{}\n\n".format(SaveParams.Section._videos)
+    for video in playlist.get_videos():
+        data += __join_line_attrs(video.get_hash(), video, _VIDEO_ATTR)
+
+    # Write the file
+    with open(playlist.get_save_path(), mode='w', encoding='utf-8') as f:
+        f.write(data)
+
+
+def __load_settings(playlist, file_lines):
+    random = False
+    keep_playing = False
+    start_at = VideoPosition._start
+    audio_track = 0
+    subtitles_track = 0
+    current_video_hash = ""
+
+    for line in __get_section_content(file_lines, SaveParams.Section._settings):
+
+        if _VALUE_SEPARATOR not in line:
+            print("Error parsing header, line=", line)
+            continue
+
+        param_name = line.split(_VALUE_SEPARATOR)[0].strip()
+        value = line.split(_VALUE_SEPARATOR)[1].strip()
+
+        match param_name:
+
+            case "random":
+                try:
+                    random = str_to_boolean(value)
+                except Exception:
+                    print("\tError getting 'random'")
+
+            case "keep_playing":
+                try:
+                    keep_playing = str_to_boolean(value)
+                except Exception:
+                    print("\tError getting 'keep_playing'")
+
+            case 'start_at':
+                try:
+                    start_at = float(value)
+                except Exception:
+                    print("\tError getting 'start_at'")
+
+            case 'audio_track':
+                try:
+                    audio_track = int(value)
+                except Exception:
+                    print("\tError getting 'audio_track'")
+
+            case 'subtitles_track':
+                try:
+                    subtitles_track = int(value)
+                except Exception:
+                    print("\tError getting 'subtitles_track'")
+                    subtitles_track = 0
+
+            case 'current_video_hash':
+                try:
+                    current_video_hash = value
+                except Exception:
+                    print("\tError getting 'current_video_hash'")
+                else:
+                    if len(current_video_hash) < 20:  # in case the CSV parser saves "false" as value
+                        current_video_hash = ""
+            case _:
+                print("Error: wrong attr name on line=", line)
+
+    #
+    # Create the playlist (without loading the videos)
+    #
+    playlist.set_random(random)
+    playlist.set_keep_playing(keep_playing)
+    playlist.set_audio_track(audio_track)
+    playlist.set_subtitles_track(subtitles_track)
+    playlist.set_start_at(start_at)
+    playlist.set_current_video_hash(current_video_hash)
+
+
+def __load_paths(file_lines):
+    paths = []
+
+    for line in __get_section_content(file_lines, SaveParams.Section._sources):
+
+        recursive = False
+        r_startup = False
+
+        #
+        # Read the data
+        #
+        columns = line.split(_COLUMN_SEPARATOR)
+
+        data_path = columns[0].strip()
+        if "/" not in data_path and "\\" not in data_path:
+            print("\tError: invalid path", line)
+            continue
+        columns.pop(0)
+
+        # Read the rest of the columns
+        for column in columns:
+            if _VALUE_SEPARATOR not in column:
+                print("\tWarning: Path with invalid column.", line)
+
+            parameter = column.split(_VALUE_SEPARATOR)[0].strip()
+            value = column.split(_VALUE_SEPARATOR)[1].strip()
+
+            match parameter:
+                case "recursive":
+                    try:
+                        recursive = str_to_boolean(value)
+                    except Exception:
+                        print("\tEWarning: Path with invalid recursive", line)
+
+                case "startup_discover":
+                    try:
+                        r_startup = str_to_boolean(value)
+                    except Exception:
+                        print("\tWarning: Path with invalid startup_discover", line)
+
+                case _:
+                    print("\tWarning: Path with ignored parameter", line)
+
+        playlist_path = PlaylistPath(path=data_path,
+                                     recursive=recursive,
+                                     startup_discover=r_startup)
+
+        paths.append(playlist_path)
+
+    return paths
+
+
+def __load_videos(playlist, file_lines):
+    imported_hash_paths = {}
+
+    for line in __get_section_content(file_lines, SaveParams.Section._videos):
+
+        path = ""
+        name = ""
+        position = VideoPosition._start
+        ignore = False
+
+        #
+        # Read the data
+        #
+        columns = line.split(_COLUMN_SEPARATOR)
+
+        # Since hashes are the ID of videos, if there's no hash, the
+        # video shall be rejected.
+        hash_file = columns[0].strip()
+        if len(hash_file) < 20:
+            print("\tError: Video with invalid hash.", line)
+            continue
+
+        columns.pop(0)
+
+        # Read the rest of the columns
+        for column in columns:
+            if _VALUE_SEPARATOR not in column:
+                print("\tWarning: Video with invalid column.", line)
+
+            parameter = column.split(_VALUE_SEPARATOR)[0].strip()
+            value = column.split(_VALUE_SEPARATOR)[1].strip()
+
+            match parameter:
+                case "path":
+                    if "/" in value or "\\" in value:
+                        path = value
+                    else:
+                        print("\tError: Video with invalid path.", line)
+                        continue
+
+                case "name":
+                    name = value
+
+                case "position":
+                    try:
+                        position = float(value)
+                    except Exception:
+                        print("\tWarning: Video with invalid position.", line)
+
+                case "ignore":
+                    try:
+                        ignore = str_to_boolean(value)
+                    except Exception:
+                        print("\tWarning: Video with invalid ignore state", line)
+
+                case _:
+                    print("\tWarning: Video with ignored parameter", line)
+
+        #
+        # Check for valid lines
+        #
+        if path == "":
+            print("\tError: Video without path.", line)
+            continue
+
+        # This test was removed to improve the time when starting the software.
+        # Normally this filter it was already applied when importing the videos
+        # for the first time.
+        #
+        # elif os.path.exists(path) and not __file_is_video(path, True):
+        #    print("\t\tSkipping line because not video.", columns)
+        #    continue
+
+        elif path in imported_hash_paths.values():
+            print("\tError: Video already path added", line)
+            continue
+
+        elif hash_file in imported_hash_paths.keys():
+            print("\tError: Video hash already imported", hash_file)
+            print("\t\tImported path:", imported_hash_paths[hash_file])
+            print("\t\tSkipped path:", path)
+            continue
+
+        video = Video(path, name)
+        video.set_position(position)
+        video.set_ignore(ignore)
+        video.set_hash(hash_file)
+        playlist.add_video(video)
+        imported_hash_paths[hash_file] = path
+
+
+def __join_line_attrs(obj_id, obj, attr_list):
+    line_data = obj_id
+    for attr_name in attr_list:
+        value = getattr(obj, "get_" + attr_name)()
+        line_data += _COLUMN_SEPARATOR + "{}={}".format(attr_name, value)
+
+    return line_data + "\n"
+
+
+def __get_section_content(lines, section_name):
+    content = []
+
+    section_found = False
+    for line in lines:
+        if line == section_name:
+            section_found = True
+
+        elif section_found:
+            if line.startswith("["):
+                break
+            else:
+                content.append(line)
+
+    return content
+
+
+def __get_lines(file_path):
+    with open(file_path, mode='rt', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    clean_lines = []
+    for line in lines:
+        line = line.strip()
+
+        if not line.startswith("#") and line != "":
+            clean_lines.append(line)
+
+    return clean_lines
--- usr/share/phantom-player/controller/video_factory.py
+++ usr/share/phantom-player/controller/video_factory.py
@@ -19,27 +19,13 @@ import os
 import magic
 import hashlib
 
-from model.Video import Video, VideoPosition
-from controller.utils import str_to_boolean, read_lines
+from model.Video import Video
 
 _MAGIC_MIMETYPE = magic.open(magic.MAGIC_MIME)
 _MAGIC_MIMETYPE.load()
 
 
-def __hash_of_file(file_path):
-    with open(file_path, "rb") as f:
-        file_hash = hashlib.sha256()
-        while chunk := f.read(8192):
-            file_hash.update(chunk)
-
-    return file_hash.hexdigest()
-
-
-def load(playlist, is_startup, add_func=None):
-    print("Loading videos of '{}':".format(playlist.get_name()))
-    load_cached(playlist, add_func=add_func)
-
-    # discover new videos
+def discover_all(playlist, is_startup, add_func=None):
     auto_discover = False
     for playlist_path in playlist.get_playlist_paths():
         if playlist_path.get_startup_discover():
@@ -50,110 +36,8 @@ def load(playlist, is_startup, add_func=None):
         discover(playlist, add_func=add_func)
 
 
-def load_cached(playlist, add_func=None):
-    if not os.path.exists(playlist.get_save_path()):
-        print("\tCached videos... SKIP, the configuration file does not exist.")
-        return
-
-    print("\tCached videos...")
-
-    current_data = {}
-
-    for i, row in enumerate(read_lines(playlist.get_save_path())):
-
-        if i < 1:  # the first line is always the header
-            continue
-
-        #
-        # Read the data
-        #
-        columns = row.split('|')
-
-        if len(columns) <= 3:  # is not a video item
-            continue
-
-        name = ""
-        position = VideoPosition._start
-        ignore = False
-        hash_file = ""
-
-        try:
-            path = columns[0].strip()
-        except Exception:
-            print("\t\terror getting the path", columns)
-            continue
-        else:
-            if "/" not in path and "\\" not in path:
-                print("\t\tinvalid the path=", path)
-                continue
-
-        try:
-            name = columns[1].strip()
-        except Exception:
-            print("\t\terror getting the name", columns)
-
-        try:
-            position = float(columns[2])
-        except Exception:
-            print("\t\terror getting the position", columns)
-
-        try:
-            ignore = str_to_boolean(columns[3])
-        except Exception:
-            print("\t\terror getting the ignore state", columns)
-
-        #
-        # Check for valid lines
-        #
-        if path is None:
-            print("\t\tSkipping line because empty path.", columns)
-            continue
-
-        if os.path.exists(path) and not __file_is_video(path, True):
-            print("\t\tSkipping line because not video.", columns)
-            continue
-
-        if path in current_data.values():
-            print("\t\tSkipping line because duplicated path.", path)
-            continue
-
-        #
-        # This must be calculated after all the previous tests,
-        # to avoid doing unuseful calculations.
-        try:
-            hash_file = columns[4].strip()
-        except Exception:
-            print("\t\terror getting the hash", columns)
-        else:
-            # Check if it is a hash, because when the hash is empty, the value is exported as False.
-            # Why? Because if the CSV module?
-            if len(hash_file) < 10:
-                hash_file = ""
-
-        if hash_file == "" and os.path.exists(path):
-            print("\t\trecalculating video hash...", path)
-            hash_file = __hash_of_file(path)
-
-        if hash_file in current_data.keys():
-            print("\t\tSkipping line because hash exists...", hash_file)
-            print("\t\t\tImported path:", current_data[hash_file])
-            print("\t\t\tSkipped path:", path)
-            continue
-
-        video = Video(path, name)
-        video.set_position(position)
-        video.set_ignore(ignore)
-        video.set_hash(hash_file)
-        playlist.add_video(video)
-        if hash_file != "":  # The hash can be empty if the path does not exist
-            current_data[hash_file] = path
-
-        if add_func is not None:
-            add_func(playlist, video)
-
-
 def discover(playlist, playlist_paths=None, add_func=None):
-    print("\tDiscovering new videos...")
+    print("Discovering new videos of '{}'...".format(playlist.get_name()))
 
     current_data = {video.get_hash(): video.get_path() for video in playlist.get_videos()}
 
@@ -163,18 +47,18 @@ def discover(playlist, playlist_paths=None, add_func=None):
     for playlist_path in playlist_paths:
 
         if os.path.exists(playlist_path.get_path()):
-            print("\t\tLoading...", playlist_path.get_path())
+            print("\tDiscovering...", playlist_path.get_path())
         else:
-            print("\t\tSkipping...", playlist_path.get_path())
+            print("\tSkipping...", playlist_path.get_path())
             continue
 
-        for video_path in __generate_videos_list_from_directory(playlist_path.get_path(),
-                                                                playlist_path.get_recursive()):
+        for video_path in __get_videos_from_dir(playlist_path.get_path(),
+                                                playlist_path.get_recursive()):
 
             if video_path in current_data.values():
                 continue  # No message on already added videos
 
-            video_hash = __hash_of_file(video_path)
+            video_hash = __file_hash(video_path)
             if video_hash in current_data.keys():
 
                 imported_path = current_data[video_hash]
@@ -186,14 +70,14 @@ def discover(playlist, playlist_paths=None, add_func=None):
                     video.set_is_new(True)
                     current_data[video_hash] = video_path
 
-                    print("\t\t\tUpdating path of video:")
-                    print("\t\t\t\tOld path:", imported_path)
-                    print("\t\t\t\tNew path:", video_path)
+                    print("\t\tUpdating path of video:")
+                    print("\t\t\tOld path:", imported_path)
+                    print("\t\t\tNew path:", video_path)
                     continue
                 else:
-                    print("\t\t\tSkipping video because hash exists...", video_hash)
-                    print("\t\t\t\tImported path:", imported_path)
-                    print("\t\t\t\tSkipped path:", video_path)
+                    print("\t\tSkipping video because hash exists...", video_hash)
+                    print("\t\t\tImported path:", imported_path)
+                    print("\t\t\tSkipped path:", video_path)
                     continue
 
             new_video = Video(video_path)
@@ -201,7 +85,7 @@ def discover(playlist, playlist_paths=None, add_func=None):
             new_video.set_hash(video_hash)
             playlist.add_video(new_video)
             current_data[video_hash] = video_path
-            print("\t\t\tAdding...", video_path)
+            print("\t\tAdding...", video_path)
             if add_func is not None:
                 add_func(playlist, new_video)
 
@@ -221,7 +105,16 @@ def __file_is_video(path, forgive_broken_links=False):
     return False
 
 
-def __generate_videos_list_from_directory(dir_path, recursive):
+def __file_hash(file_path):
+    with open(file_path, "rb") as f:
+        file_hash = hashlib.sha256()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+
+    return file_hash.hexdigest()
+
+
+def __get_videos_from_dir(dir_path, recursive):
     if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
         return []
 
