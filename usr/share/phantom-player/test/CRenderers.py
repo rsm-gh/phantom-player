af088f5bed7c281b3208cd373a2c5bbe6d5a0c54
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-07-08 22:32:34 +0200

    +

--- usr/share/phantom-player/test/CRenderers.py
+++ usr/share/phantom-player/test/CRenderers.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-#
 
-#  Copyright (C) 2016 Rafael Senties Martinelli
 #
-#  This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License 3 as published by
-#   the Free Software Foundation.
+# MIT License
 #
-#  This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# Copyright (c) 2014-2016, 2024 Rafael Senties Martinelli.
 #
-# You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software Foundation,
-#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-
-"""
-    This is one part of the code that I’m pretty sure that it could be improved.
-    I don’t know much about cellrenderers. Great doc at: 
-    
-        - http://python-gtk-3-tutorial.readthedocs.org/en/latest/objects.html
-        - http://zetcode.com/gui/pygtk/signals/ https://lazka.github.io/pgi-docs/Gtk-3.0/classes/CellRenderer.html
-"""
-import os.path
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import gi
-
 gi.require_version('Gtk', '3.0')
 gi.require_version('PangoCairo', '1.0')  # necessary for the cell renderers
 from gi.repository import Gtk
