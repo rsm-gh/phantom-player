ed433b2a697bfcaabd1281c2db423f3f24022238
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-09-27 21:03:28 +0200

    VLC, share the same instance

--- usr/share/phantom-player/view/PhantomPlayer.py
+++ usr/share/phantom-player/view/PhantomPlayer.py
@@ -508,7 +508,7 @@ class PhantomPlayer:
         # It is better to stop the playlists threads before quitting the media player,
         # because the VLC instance will be released.
         self.__thread_load_playlists.join()
-        vlc_utils.release()
+        vlc_utils.release_instance()
         self.__mp_widget.quit()
         self.__application.quit()
 
