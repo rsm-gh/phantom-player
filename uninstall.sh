ff6a752056a9c0a9b55ccb157f4a4913078a44fe
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2025-04-25 18:47:53 +0200

    +

--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 #
-#    This file is part of Phantom Player.
+# This file is path of Phantom Player.
 #
-# Copyright (C) 2015, 2024 Rafael Senties Martinelli.
+#  Copyright (C) 2015, 2024 Rafael Senties Martinelli.
 #
-# This work is in the public domain under the CC0 1.0 Universal license.
-# To the extent possible under law, the author has waived all copyright
-# and related or neighboring rights to this work.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# You should have received a copy of the CC0 legalcode along with this
-# work.  If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 
 if [ "$EUID" -ne 0 ]
