96692ef92001e5262f8a348684be71099e984e19
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2025-05-17 16:30:32 +0200

    License compliance, specify GPL3 or LGPL2.1

--- usr/share/phantom-player/model/Playlist.py
+++ usr/share/phantom-player/model/Playlist.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 
 #
-#    This file is part of Phantom Player.
+#   This file is part of Phantom Player.
 #
 # Copyright (c) 2014-2016, 2024-2025 Rafael Senties Martinelli.
 #
-#  This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU Lesser General Public License 2.1 as
-#   published by the Free Software Foundation.
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of either:
 #
-#  This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Lesser General Public License for more details.
+#   - the GNU Lesser General Public License as published by
+#     the Free Software Foundation, version 2.1 only, or
 #
-#  You should have received a copy of the GNU Lesser General Public License
-#   along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.en.html>.
+#   - the GNU General Public License as published by
+#     the Free Software Foundation, version 3 only.
 #
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the applicable licenses for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 2.1 and the GNU General Public License version 3
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: LGPL-2.1-only OR GPL-3.0-only
 
 import os
 import random
